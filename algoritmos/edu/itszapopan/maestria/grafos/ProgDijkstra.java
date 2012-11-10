636d95a61f9e13a7b8823a627bb587699e344e0c
Ramses Lafarga <ram.lafarga@gmail.com>
2012-11-10 11:12:34 -0600

    le borre todo

--- algoritmos/edu/itszapopan/maestria/grafos/ProgDijkstra.java
+++ algoritmos/edu/itszapopan/maestria/grafos/ProgDijkstra.java
@@ -7,18 +7,6 @@ public class ProgDijkstra {
 
     dg = new Digrafo(10);
 
-    dg.setArista(0,"a");
-    dg.setArista(1,"b");
-    dg.setArista(2,"c");
-    dg.setArista(3,"d");
-    dg.setArista(4,"e");
-    dg.setArista(5,"f");
-    dg.setArista(6,"g");
-    dg.setArista(7,"h");
-    dg.setArista(8,"i");
-    dg.setArista(9,"j");
-
-    dg.setConexion("a","e",1);
 
 
   }
